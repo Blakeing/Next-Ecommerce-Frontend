5df92a8613eaa27c9318710d9b545a48e999f3b0
Blake Ingenthron <blakeingenthron@gmail.com>
2020-12-31 16:45:07 -0600

    intial commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
-import { fromImageToUrl, API_URL } from '../utils/urls'
-import { twoDecimals } from '../utils/format'
+import { fromImageToUrl, API_URL } from "../utils/urls";
+import { twoDecimals } from "../utils/format";
 
 export default function Home({ products }) {
   return (
     <div>
       <Head>
         <title>Build an Ecommerce with NextJS, Magic, Strapi and Stripe</title>
-        <meta name="description" content="Learn how to build a FullStack Ecommerce in this 2 hours and a half free video sponsored by Magic" />
+        <meta
+          name="description"
+          content="Learn how to build a FullStack Ecommerce in this 2 hours and a half free video sponsored by Magic"
+        />
       </Head>
 
-      {products.map(product => (
-        <div className={styles.product}>
+      {products.map((product) => (
+        <div key={product.name} className={styles.product}>
           <Link href={`/products/${product.slug}`}>
             <a>
               <div className={styles.product__Rows}>
@@ -30,16 +33,16 @@ export default function Home({ products }) {
         </div>
       ))}
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const product_res = await fetch(`${API_URL}/products/`)
-  const products = await product_res.json()
+  const product_res = await fetch(`${API_URL}/products/`);
+  const products = await product_res.json();
 
   return {
     props: {
-        products
-    }
-  }
+      products,
+    },
+  };
 }
--- utils/urls.js
+++ utils/urls.js
@@ -1,23 +1,26 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1338"
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 
-export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || 'pk_test_42B2064C668798B5'
+export const MAGIC_PUBLIC_KEY =
+  process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || "pk_test_E148540E07EAB4B3";
 
-export const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PK || 'pk_test_42B2064C668798B5'
+export const STRIPE_PK =
+  process.env.NEXT_PUBLIC_STRIPE_PK ||
+  "pk_test_51I4WkFGiR9lVh1FZkYkuTDo5rp6iAySc8JxCeZDo6ri9n1NLqMGEqippXMkQFrOHvyfsmMlB3u3veErYHz9aqUTX001cwyTwCO";
 
 /**
  * Given a image object return the proper path to display it
  * Provides a default as well
- * @param {any} image 
+ * @param {any} image
  */
 export const fromImageToUrl = (image) => {
-    if (!image) {
-      return "/vercel.svg"; //Or default image here
-    }
-    if (image.url.indexOf("/") === 0) {
-      //It's a relative url, add API URL
-      return `${API_URL}${image.url}`;
-    }
-  
-    return image.url;
+  if (!image) {
+    return "/vercel.svg"; //Or default image here
+  }
+  if (image.url.indexOf("/") === 0) {
+    //It's a relative url, add API URL
+    return `${API_URL}${image.url}`;
+  }
+
+  return image.url;
 };
